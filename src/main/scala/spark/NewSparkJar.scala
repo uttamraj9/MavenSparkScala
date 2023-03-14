d7bb3dbe5e8707794405d3ebd58510fbc1236602
uttamraj9 <uttamraj9@gmail.com>
2023-03-14 19:16:14 +0000

    First Commit

---
+++ src/main/scala/spark/NewSparkJar.scala
@@ -0,0 +1,16 @@
+package spark
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object NewSparkJar {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("someName").setAppName("WordCount").setMaster("local")
+    //create spark context object
+    val sc = new SparkContext(conf)
+
+    //Create RDD from parallelize
+    val dataSeq = Seq(("Java", 20000), ("Python", 100000), ("Scala", 3000))
+    val rdd = sc.parallelize(dataSeq)
+    println(rdd.collect().foreach(print))
+  }
+}
